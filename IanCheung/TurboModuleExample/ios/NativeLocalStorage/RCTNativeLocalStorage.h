6f374558e53ba1e37c7096c15c575c7c329770e6
Ian Cheung <ij4.cheung@gmail.com>
2025-09-28 20:43:49 -0400

    created iOS app

---
+++ IanCheung/TurboModuleExample/ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleExample
+//
+//  Created by Ian Cheung on 9/28/25.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
