8b8052b0455b600cd15f579fa87eda3606d7ad8a
underratedicon <chukwup1@southernct.edu>
2025-09-30 10:28:51 -0400

    Fix MyFirstApp added as regular folder instead of submodule

---
+++ Chukwu_Peter_week3-4/MyFirstApp/MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Ebube Dike on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chukwu_Peter_week3-4/MyFirstApp/MyFirstAppTests/MyFirstAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyFirstAppTests.swift
+//  MyFirstAppTests
+//
+//  Created by Ebube Dike on 9/11/25.
+//
+
+import Testing
+
+struct MyFirstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
