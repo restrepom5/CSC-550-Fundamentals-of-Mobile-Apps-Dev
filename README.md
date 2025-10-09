36fae8d4af10a142379b46af5f3a5b344d29a079
Tiffany Choe <tchoe@aquaphoenixsci.com>
2025-10-08 23:20:21 -0400

    native module

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CSC-550-Fundamentals-of-Mobile-Apps-Dev
 Fall 2025
+
+1. ```cd choe_tiffany_week3-4/TurboModuleExample```
+2. in one terminal run ```npm run start```
+3. in another terminal run ```npm run ios``` or ```npm run android```
---
+++ choe_tiffany_week3-4/TurboModuleExample/ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleExample
+//
+//  Created by Tiffany Choe on 10/8/25.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
